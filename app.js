41652bc7c3b0faf66f74f3b0fe5f8a1f42944b2a
zeman <zemanqwe@gmail.com>
2023-07-14 02:38:59 +0800

    feat: добавление возможности передвижения при нажатии клавиш

--- app.js
+++ app.js
@@ -9,9 +9,10 @@ import { fileURLToPath } from 'url';
 const app = express();
 const port = 2001;
 const server = createServer(app);
-const io = new Server(server, {})
+const io = new Server(server, {});
 
 var socketsList = {};
+var playersList = {};
 
 // const socket = io.sockets;
 
@@ -20,18 +21,68 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 
+const Player = (id) => {
+    let self = {
+        x: 250,
+        y: 250,
+        number: Math.floor(10 * Math.random()).toString(),
+        prsUp: false,
+        prsDown: false,
+        prsRight: false,
+        prsLeft: false,
+        maxSpeed: 10,
+    }
+
+    self.updatePosition = () => {
+        if (self.prsUp) {
+            self.y -= self.maxSpeed;
+        }
+        if (self.prsDown) {
+            self.y += self.maxSpeed;
+        }
+        if (self.prsRight) {
+            self.x += self.maxSpeed;
+        }
+        if (self.prsLeft) {
+            self.x -= self.maxSpeed;
+        }
+    }
+
+    return self;
+};
+
+
 io.sockets.on('connection', (socket) => {
     console.log('connected to socket io');
 
     socket.id = Math.random();
-    socket.x = 0;
-    socket.y = 0;
-    socket.number = '' + Math.floor(10 * Math.random())
+    // socket.x = 0;
+    // socket.y = 0;
+    // socket.number = '' + Math.floor(10 * Math.random())
 
+    let player = Player(socket.id);
+
+    playersList[socket.id] = player;
     socketsList[socket.id] = socket;
+
+    socket.on('KeyPress', (data) => {
+        if (data.inputId == 'right') {
+            player.prsRight = data.state;
+        }
+        else if (data.inputId == 'left') {
+            player.prsLeft = data.state;
+        }
+        else if (data.inputId == 'up') {
+            player.prsUp = data.state;
+        }
+        else if (data.inputId == 'down') {
+            player.prsDown = data.state;
+        }
+    })
     
     socket.on('disconnect', () => {
         delete socketsList[socket.id];
+        delete playersList[socket.id];
     });
 });
 
@@ -39,16 +90,15 @@ io.sockets.on('connection', (socket) => {
 setInterval(() => {
     let pack = [];
 
-    for (let i in socketsList) {
-        let sockClient = socketsList[i];
+    for (let i in playersList) {
+        let player = playersList[i];
 
-        sockClient.x++;
-        sockClient.y++;
+        player.updatePosition();
 
         pack.push({
-            x: sockClient.x,
-            y: sockClient.y,
-            number: sockClient.number
+            x: player.x,
+            y: player.y,
+            number: player.number
         })
     };
 
