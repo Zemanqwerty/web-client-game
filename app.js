ff375275a600b67baad75e02f0e87de455565e78
zeman <zemanqwe@gmail.com>
2023-07-13 16:24:11 +0800

    feat: добавление броадкаст рассылки

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const port = 2001;
 const server = createServer(app);
 const io = new Server(server, {})
 
+var socketsList = {};
+
 // const socket = io.sockets;
 
 
@@ -21,14 +23,40 @@ const __dirname = path.dirname(__filename);
 io.sockets.on('connection', (socket) => {
     console.log('connected to socket io');
 
-    socket.on('happy', (data) => {
-        console.log(data.message);
-    });
+    socket.id = Math.random();
+    socket.x = 0;
+    socket.y = 0;
+    socket.number = '' + Math.floor(10 * Math.random())
 
-    socket.emit('serverMsg', {
-        message: 'message from server'
-    })
+    socketsList[socket.id] = socket;
+    
+    socket.on('disconnect', () => {
+        delete socketsList[socket.id];
+    });
 });
+
+
+setInterval(() => {
+    let pack = [];
+
+    for (let i in socketsList) {
+        let sockClient = socketsList[i];
+
+        sockClient.x++;
+        sockClient.y++;
+
+        pack.push({
+            x: sockClient.x,
+            y: sockClient.y,
+            number: sockClient.number
+        })
+    };
+
+    for (let i in socketsList) {
+        let sockClient = socketsList[i];
+        sockClient.emit('newPosition', pack)
+    };
+}, 1000 / 25);
 
 
 
